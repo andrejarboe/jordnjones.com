edc5b2dabe00e3ec9e879555a42346916302ccfe
Jordan Jones <proxima.aust@gmail.com>
2019-01-09 13:51:55 -0500

    Added extra build folder for github pages

---
+++ docs/component---src-pages-404-js-ab19e23e2e79b7f5899b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{138:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(150),o=n(149);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ab19e23e2e79b7f5899b.js.map
